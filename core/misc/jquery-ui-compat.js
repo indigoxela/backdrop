d0856782fabfe7f85f5f36947a2a917bd9a9f8c2
indigoxela <user@olga.indigofloat.at>
2024-08-06 14:03:54 +0200

    Another attempt to fix grammar in comment block

--- core/misc/jquery-ui-compat.js
+++ core/misc/jquery-ui-compat.js
@@ -2,8 +2,8 @@
  * @file
  * Sets the compatibility flag for jQuery UI back to true.
  *
- * Core still uses deprecated "dialogClass" option, which is deprecated and
- * since jQuery UI version 1.14.0 this flag defaults to false.
+ * Core still uses "dialogClass" option, which is deprecated and since jQuery UI
+ * version 1.14.0 this flag defaults to false.
  */
 (function ($) {
   $.uiBackCompat = true;
